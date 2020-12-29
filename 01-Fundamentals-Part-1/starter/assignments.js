eabe0c933835fb88af8daab6c247153765667f71
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-29 18:02:45 -0500

    assigment let const  and data types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,10 @@
-let country, continent, population;
+let country, continent, population, isIsland;
 
 country = 'México';
 continent = 'America';
 population = 128932753;
+isIsland = true;
+const language = 'español';
 
 console.log(`I'm from ${country} that is in ${continent} and I'm one of the ${population} persons that live in this country `);
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,4 +7,15 @@ console.log(23);
 let firsName = 'Gerardo';
 console.log(firsName);
 let PI = 3.1415;
-let miPrimerTrabajo;
+let miPrimerTrabajo = 'diseñador';
+let miTrabajoActual = 'Senior developer';
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'true');
+
+JavascriptIsFun = 'Yes!';
+console.log(typeof JavascriptIsFun);
