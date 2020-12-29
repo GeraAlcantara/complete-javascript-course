0e123bde8b98b810b7a186f6f70be891254fbdf7
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-29 17:42:08 -0500

    assigment value varibles

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country, continent, population;
+
+country = "México";
+continent = "America";
+population = 128932753;
+
+console.log(`I'm from ${country} that is in ${continent} and I'm one of the ${population} person that live in this country `);
