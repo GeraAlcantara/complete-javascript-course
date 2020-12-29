79d468b192f465fe948569d2f769db9e7c3a95c5
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-29 18:40:55 -0500

    code chalenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,8 @@
-let js = 'amazing';
+/* let js = 'amazing'; */
 /* if (js === "amazing") {
   alert("Javascript is FUN !");
 } */
-console.log(40 + 8 + 23 - 10);
+/* console.log(40 + 8 + 23 - 10);
 console.log(23);
 let firsName = 'Gerardo';
 console.log(firsName);
@@ -18,4 +18,49 @@ console.log(typeof 23);
 console.log(typeof 'true');
 
 JavascriptIsFun = 'Yes!';
-console.log(typeof JavascriptIsFun);
+console.log(typeof JavascriptIsFun); */
+
+// Math Oparators
+/* const currentyear = 2020;
+const ageGera = currentyear - 1979;
+const ageMariana = currentyear - 1986;
+console.log(ageGera, ageMariana);
+
+console.log(ageGera * 2, ageGera / 10, 2 ** 3); */
+// ** 3 significa a la tercera potencia = n *n * n
+
+/* const primerNombre = 'Gerardo';
+const apellido = 'Alcantara';
+
+console.log(primerNombre + ' ' + apellido);
+console.log(`${primerNombre} ${apellido}`); */
+
+// Assigment operators
+/* let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x) */ // comparecen Operators
+
+// >,  <, >=, <=
+/* console.log(ageGera > ageMariana);
+console.log(ageGera < ageMariana);
+console.log(ageGera === ageMariana); */
+
+/* const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95; */
+
+// DATA 2
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
