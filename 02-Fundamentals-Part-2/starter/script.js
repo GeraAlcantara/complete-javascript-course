b8dd3fb2586ac0d4ae1a35a2fe516afd015a8bdd
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-30 17:18:15 -0500

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,3 +6,23 @@ const passTest = true;
 
 if (passTest) hasDriverLicense = true;
 if (hasDriverLicense) console.log('I can drive');
+
+function logger() {
+  console.log('My name is Gerardo');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges `;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
