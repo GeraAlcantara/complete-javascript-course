a98353307f0f03fe56689abdf423f87aabbe05bd
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-30 15:50:24 -0500

    Challange 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -105,7 +105,7 @@ const massJohn = 92;
 const heightJohn = 1.95; */
 
 // DATA 2
-const massMark = 95;
+/* const massMark = 95;
 const heightMark = 1.88;
 const massJohn = 85;
 const heightJohn = 1.76;
@@ -119,4 +119,82 @@ if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})! `);
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})! `);
+} */
+
+// Type conversion
+/* const inputYear = '1991';
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18);
+console.log(Number('jonas'));
+console.log(String(23));
+
+//Type coercion
+console.log("I'm " + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18'); */
+
+// falsy values 0, '', undefined, null, NaN
+/* let n = 0;
+if (n) {
+  console.log(true);
+} else {
+  console.log(false);
+} */
+
+/* const age = 18;
+
+if (age === 18) {
+  console.debug(' You just become an adult');
+} */
+
+/* const hasDriverLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+if (hasGoodVision && hasGoodVision) {
+  console.log(` sara is able to drive`);
+} else {
+  console.log(` someone else should drive`);
+} */
+/* Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK */
+
+const scoreDolphines = 106 + 109 + 95;
+const scoreKoalas = 109 + 95 + 106;
+
+const averageDolphines = scoreDolphines / 3;
+const averageKoalas = scoreKoalas / 3;
+
+if (averageDolphines > 100 || averageKoalas > 100) {
+  if (averageDolphines === averageKoalas) {
+    console.log('Es un empate');
+  } else if (averageDolphines > averageKoalas) {
+    console.log('Dolphines wins');
+  } else {
+    console.log('Koalas wins!');
+  }
+} else {
+  console.log('no team score more than 100');
 }
