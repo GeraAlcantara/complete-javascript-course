ad0b97da8f349a34dcf0a9f4a20785032d55c9d5
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-12-31 16:25:42 -0500

    debuge in vs code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,48 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-const x = '23';
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const temperatures2 = [5, -40, -6, -1, 'error', 9, 13, 17, 15, 14, 90, 5];
+
+const calcTempAmplitud = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') {
+      continue;
+    }
+    if (currentTemp > max) {
+      max = currentTemp;
+    }
+    if (currentTemp < min) {
+      min = currentTemp;
+    }
+  }
+  return max - min;
+};
+const amplitude = calcTempAmplitud(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') {
+      continue;
+    }
+    if (currentTemp > max) {
+      max = currentTemp;
+    }
+    if (currentTemp < min) {
+      min = currentTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudNew(temperatures, temperatures2);
+console.log(amplitudeNew);
