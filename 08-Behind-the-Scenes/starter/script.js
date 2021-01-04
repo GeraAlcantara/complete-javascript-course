beaa09a819acbcaf7c81f1ede84315c4c8ddd5c7
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-01-04 12:12:40 -0500

    this and references

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+/* const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    const self = this;
+    console.log(2037 - this.year);
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+  greet: () => {
+    console.log(this);
+    console.log(`hey ${this.firstName} `);
+  },
+};
+
+jonas.greet();
+jonas.calcAge(); */
+
+const jesica = {
+  firstName: 'jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJesica = Object.assign({}, jesica);
+marriedJesica.lastName = 'davis';
+console.log(jesica, marriedJesica);
