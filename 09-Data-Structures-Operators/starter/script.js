57b2402c424026a807837e7714c4033641a31101
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-01-06 13:26:21 -0500

    challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -7,40 +22,209 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  openingHours,
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderDelevery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelevery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order recives! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delevered to ${address} at ${time} `
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Heres is your delicious pasta with ${ing1}, ${ing2}, ${ing3} `
     );
   },
 };
 
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+//2.
+gameEvents.delete(64);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+//4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(` [${half} HALF] ${min}: ${event} `);
+}
+
+// Data Structure Wich One
+
+/* const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]); */
+
+/* console.log(Object.entries(openingHours)); */
+/* const hoursMap = new Map(Object.entries(openingHours)); */
+/* console.log(question);
+console.log(hoursMap); */
+
+// Question App
+/* console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value} `);
+  }
+} */
+/* const answer = Number(prompt('Your answer'));
+console.log(question.get(question.get('correct') === answer)); */
+/* console.log(...question); */
+
+// /////  Maps Fundamentals
+
+/* const rest = new Map();
+rest.set('name', 'Clasico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open 🍕')
+  .set(false, 'We are close 👈');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+// key can be DOM Elements
+rest.set(document.querySelector('h1'), 'Heading');
+const arr = [1, 2];
+rest.set(arr, 'test');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr)); */
+// Sets
+
+/* const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risoto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(orderSet);
+
+console.log(new Set('gera'));
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+orderSet.delete('Risoto');
+// orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+console.log(new Set('jonasschmedtmann').size); */
+
+/* const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+} */
+
+/* const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) {
+  console.log(item);
+}
+
+
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open} `);
+}
+
+// methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisoto?.(0, 1) ?? 'Method does not exist');
+
+// const users = [{ name: 'jonas', email: 'hello@jonas.io' };
+const users = [];
+
+console.log(users[0]?.name ?? 'user array empty');
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty'); */
 /* const ingredients = [
   prompt(`Let's make pasta!, Ingredient 1?`),
   prompt('Ingredient 2 '),
@@ -51,14 +235,14 @@ restaurant.orderPasta(...ingredients); */
 
 // objects only in ES2018
 
-const newRestaurant = { founded: 1998, ...restaurant, founder: 'Guiseppe' };
+/* const newRestaurant = { founded: 1998, ...restaurant, founder: 'Guiseppe' };
 
 console.log(newRestaurant);
 
 const restauranCopy = { ...restaurant };
 restauranCopy.name = 'Ristorante Roma';
 console.log(restaurant.name);
-console.log(restauranCopy.name);
+console.log(restauranCopy.name); */
 
 /* const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -157,3 +341,113 @@ console.log(i, j, k);
 // destructuring with Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r); */
+
+/* const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: [
+    'Lewandowski',
+    'Gnarby',
+    'Gnarby',
+    'Gnarby',
+    'Lewandowski',
+    'Hummels',
+  ],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+}; */
+//////////////////// chalenge #2
+
+/* BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
+} */
+
+/* const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+
+console.log(scorers);
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player} `);
+}
+let avgOdds = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds) {
+  avgOdds += odd;
+}
+avgOdds /= odds.length;
+console.log(avgOdds);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]} `;
+  console.log(`Odd of ${teamStr} ${odd} `);
+} */
+/* const [players1, players2] = game.players;
+// console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allplayers = [...players1, ...players2];
+
+console.log(allplayers);
+
+const players1final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+console.log(players1final);
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...playersnames) {
+  for (let i = 0; i < playersnames.length; i++) {
+    const player = playersnames[i];
+    console.log(`${player} `);
+  }
+  console.log(`the total goal were ${playersnames.length} `);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+console.log(
+  `the team more likely to win is ${
+    (game.odds.team1 < game.odds.team2 && game.team1) ||
+    (game.odds.team2 < game.odds.team1 && game.team2)
+  } `
+); */
