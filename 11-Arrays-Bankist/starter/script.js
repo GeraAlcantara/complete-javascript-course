c82940eecca51a3f282b55123abcea1d84cbc80c
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-01-08 18:19:18 -0500

    chain methods on arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,211 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type} </div>
+      <div class="movements__value">${mov} </div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const createUserNames = accs => {
+  accs.forEach(acc => {
+    acc.userName = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+const user = 'Steven Thomas Williams'; // stw
+createUserNames(accounts);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+//1.
+/* const dataSet1 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (ages) {
+  const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAge.filter(age => age >= 18);
+  console.log(humanAge);
+  console.log(adults);
+
+  const avg = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  // const avg = adults.reduce((acc, age, _, arr) => acc + age / arr.length, 0);
+  return avg;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2); */
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const euroToUSD = 1.1;
+
+// PIPELINE
+const totalDepositinUISD = movements
+  .filter(mov => mov > 0)
+  // debug to check the operaation with the arr parameter
+  .map((mov, _, arr) => {
+    /* console.log(arr); */
+    return mov * euroToUSD;
+  })
+  .reduce((acc, cur) => acc + cur);
+
+console.log(totalDepositinUISD);
+
+// Reduce
+// ACCUMULATOR --> CURRENT
+/* const totalMovements = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(totalMovements);
+
+const maxValueMovements = movements.reduce(
+  (acc, mov) => (mov > acc ? mov : acc),
+  movements[0]
+);
+console.log(maxValueMovements);
+
+// filter
+const deposit = movements.filter(mov => mov > 0);
+console.log(deposit);
+const withDrawals = movements.filter(mov => mov < 0);
+console.log(withDrawals); */
+
+// MAP
+
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You deposited ${
+      mov > 0 ? 'deposited' : 'withdres'
+    } ${Math.abs(mov)}`
+);
+console.log(movDescriptions); */
+
+/* const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]); */
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/* let arr = ['a', 'b', 'c', 'd', 'e']; */
+
+// SLICE
+/* console.log(arr.slice(2));
+console.log(arr.slice(2, arr.length - 1));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]); */
+
+// SPLICE
+
+// console.log(arr.splice(2));
+/* arr.splice(-1);
+console.log(arr.splice(1, 2));
+console.log(arr); */
+
+// REVERSE
+/* const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2);
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - ')); */
+
+// FOREACH
+
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdres ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----- FOREACH ------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+    console.log(arr);
+  } else {
+    console.log(`Movement ${i + 1}: You withdres ${Math.abs(mov)}`);
+  }
+}); */
+
+/* const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
 
-/////////////////////////////////////////////////
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${_}: ${value}`);
+});
+ */
