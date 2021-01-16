2dfde1a3b3a025ef95ff67ba6c2a26aa452248d9
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-01-16 14:13:19 -0500

    building Class App Arquitectura

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,74 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+  constructor() {
+    this._getPosition();
+    this._map;
+    this._mapEvent;
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevetionField);
+  }
+
+  _getPosition() {
+    console.log(this);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position');
+        }
+      );
+    }
+  }
+  _loadMap(position) {
+    const latitude = 20.652653434086066;
+    const longitude = -87.102130651474;
+    console.log(`https://www.google.com.mx/maps/@${latitude},${longitude}`);
+    console.log(position);
+    const coords = [latitude, longitude];
+    console.log(this);
+    this._map = L.map('map').setView(coords, 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this._map);
+
+    //Handling click on map
+
+    this._map.on('click', this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    this._mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevetionField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+
+    //clear Input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+      '';
+    const { lat, lng } = this._mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this._map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
+  }
+}
+const app = new App();
